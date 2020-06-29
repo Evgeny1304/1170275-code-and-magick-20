7cd3e28deb7c6486ba28f76f2fef6363cb8ddf52
Evgeny1304 <erhabirov@gmail.com>
2020-06-29 20:50:32 +0300

    Корректировка кода

--- js/dialog.js
+++ js/dialog.js
@@ -6,6 +6,7 @@
   var setupClose = document.querySelector('.setup-close');
   var userNameInput = document.querySelector('.setup-user-name');
   var similarWizardList = document.querySelector('.setup-similar-list');
+  var dialogHandle = setupPopup.querySelector('.upload');
 
   var setupPopupTop = window.getComputedStyle(setupPopup).getPropertyValue('top');
   var setupPopupLeft = window.getComputedStyle(setupPopup).getPropertyValue('left');
@@ -47,5 +48,54 @@
 
   setupClose.addEventListener('keydown', function (evt) {
     window.util.isEnterEvent(evt, closeSetupPopup);
+  });
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setupPopup.style.top = (setupPopup.offsetTop - shift.y) + 'px';
+      setupPopup.style.left = (setupPopup.offsetLeft - shift.x) + 'px';
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogHandle.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandle.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
